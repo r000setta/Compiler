304b2420616f07cac8587575e94351d892db152c
rosetta404 <1509248091@qq.com>
2019-11-15 12:58:31 +0800

    Initial commit

---
+++ src/Lexical.java
@@ -0,0 +1,10 @@
+public class Lexical {
+    private static String[] tokens={"do","while"};
+
+    private void analyze(String tokenStream){
+        
+    }
+    public static void main(String[] args) {
+
+    }
+}
